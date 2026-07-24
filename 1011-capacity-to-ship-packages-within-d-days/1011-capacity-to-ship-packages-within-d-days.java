e17ae0c901f37bcbc225ecfc2c99c3ce93201f18
Abhishek Belwal <abhishekbelwal807@gmail.com>
2026-07-25 00:07:14 +0530

    Time: 9 ms (98.74%), Space: 50.4 MB (6.87%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,40 +1,31 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int start = 0;
-        int end = 0;
-
-        // Search space
-        for (int weight : weights) {
-            start = Math.max(start, weight);
-            end += weight;
+        int start=0;
+        int end=0;
+        for(int weight:weights){
+            start=Math.max(start,weight);
+            end+=weight;
         }
-
-        int ans = end;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            int daysUsed = 1;
-            int currentLoad = 0;
-
-            // Check if capacity = mid is possible
-            for (int weight : weights) {
-                if (currentLoad + weight <= mid) {
-                    currentLoad += weight;
-                } else {
-                    daysUsed++;
-                    currentLoad = weight;
+        int ans=end;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int useddays=1;
+            int load=0;
+            for(int weight:weights){
+                if(load+weight<=mid){
+                    load+=weight;
+                }else{
+                    useddays++;
+                    load=weight;
                 }
             }
-
-            if (daysUsed <= days) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+            if(useddays<=days){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
         }
-
         return ans;
     }
 }
