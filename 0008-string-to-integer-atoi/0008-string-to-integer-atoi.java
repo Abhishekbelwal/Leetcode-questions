144a254f9f359de7ed8826a7d6f7d5ee15946d26
Abhishek Belwal <abhishekbelwal807@gmail.com>
2026-07-30 00:16:38 +0530

    Time: 1 ms (100%), Space: 43.4 MB (95.08%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,42 +1,29 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0;
-        int n = s.length();
-
-        // 1. Skip leading whitespaces
-        while (i < n && s.charAt(i) == ' ') {
+        int n=s.length();
+        int i=0;
+        while(i<n &&s.charAt(i)==' '){
             i++;
         }
+        if(i==n) return 0;
 
-        // 2. Check if string is empty after spaces
-        if (i == n) return 0;
-
-        // 3. Check sign
-        int sign = 1;
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-            if (s.charAt(i) == '-') {
-                sign = -1;
+        int sign=1;
+        if(s.charAt(i)=='+'||s.charAt(i)=='-'){
+            if(s.charAt(i)=='-'){
+                sign=-1;
             }
             i++;
         }
-
-        // 4. Build the number
-        int result = 0;
-
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
-
-            // 5. Check overflow before multiplying
-            if (result > Integer.MAX_VALUE / 10 ||
-               (result == Integer.MAX_VALUE / 10 && digit > 7)) {
-
+        int result =0;
+        while(i<n&&Character.isDigit(s.charAt(i))){
+            int digit=s.charAt(i)-'0';
+            if(result>Integer.MAX_VALUE/10||(result==Integer.MAX_VALUE/10 && digit>7)){
                 return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
 
-            result = result * 10 + digit;
+            }
+              result = result * 10 + digit;
             i++;
         }
-
-        return result * sign;
+         return result * sign;
     }
 }
