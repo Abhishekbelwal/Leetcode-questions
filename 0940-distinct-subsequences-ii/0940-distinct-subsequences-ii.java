92c5dcca8a8547f29e42d89aaf5a15ce0bd3d536
Abhishek Belwal <274573920+Abhishekbelwal@users.noreply.github.com>
2026-09-07 22:40:24 +0530

    Time: 5 ms (54.61%), Space: 43.5 MB (41.84%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,20 +1,33 @@
 class Solution {
     public int distinctSubseqII(String s) {
-        long[] end = new long[26];
+        int n = s.length();
+        long[] dp = new long[n + 1];
 
-        long total = 0;
+        dp[0] = 1; // empty subsequence
+
+        int[] last = new int[26];
+        Arrays.fill(last, -1);
+
         long MOD = 1_000_000_007;
 
-        for (char c : s.toCharArray()) {
-            int index = c - 'a';
+        for (int i = 1; i <= n; i++) {
+            int ch = s.charAt(i - 1) - 'a';
 
-            long newSubsequences = (total + 1) % MOD;
+            // Every previous subsequence can either
+            // take or not take current character
+            dp[i] = 2 * dp[i - 1];
 
-            total = (total + newSubsequences - end[index] + MOD) % MOD;
+            // Remove duplicates created by previous occurrence
+            if (last[ch] != -1) {
+                dp[i] -= dp[last[ch] - 1];
+            }
 
-            end[index] = newSubsequences;
+            dp[i] = (dp[i] + MOD) % MOD;
+
+            last[ch] = i;
         }
 
-        return (int) total;
+        // Remove empty subsequence
+        return (int) ((dp[n] - 1 + MOD) % MOD);
     }
 }
