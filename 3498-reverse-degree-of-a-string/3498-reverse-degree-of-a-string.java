329a30eddc5b914fcb78af5693bd125acb6de5ee
Abhishek Belwal <274573920+Abhishekbelwal@users.noreply.github.com>
2026-09-20 22:42:45 +0530

    Time: 1 ms (100%), Space: 44.1 MB (34.29%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,21 +1,14 @@
 class Solution {
     public int reverseDegree(String s) {
-        int totalDegree = 0;
-        int len = s.length(); // 1. Use length() for loop boundaries
-        
-        for (int i = 0; i < len; i++) {
-            char currChar = s.charAt(i); // 2. Extract character
-            
-            // 3. Calculate reversed alphabet rank ('a'=26, 'z'=1)
-            int reverseAlphabetPos = 'z' - currChar + 1; 
-            
-            // 4. Calculate 1-indexed position in the string
-            int stringPos = i + 1; 
-            
-            // 5. Accumulate the product
-            totalDegree += reverseAlphabetPos * stringPos;
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            int reverseValue = 26 - (c - 'a');
+            int position = i + 1;
+
+            sum += reverseValue * position;
         }
-        
-        return totalDegree;
+        return sum;
     }
 }
